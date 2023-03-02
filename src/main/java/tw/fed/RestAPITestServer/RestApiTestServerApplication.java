3185eb80ec76592c12a095c7fdcc957c771399b8
fedhu <fedhu@gamasys.com.tw>
2023-03-02 16:19:02 +0800

    init

---
+++ src/main/java/tw/fed/RestAPITestServer/RestApiTestServerApplication.java
@@ -0,0 +1,14 @@
+package tw.fed.RestAPITestServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTestServerApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/tw/fed/RestAPITestServer/RestApiTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.fed.RestAPITestServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
